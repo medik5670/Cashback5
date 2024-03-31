3bc60937e18f8940479901575bfb904bc2068aaf
Naty test <nastya-medyantsevaa34@mail.ru>
2024-03-31 20:31:07 +0300

    chore: secondary commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.service;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainNoRefund() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+}
